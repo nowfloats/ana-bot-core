c3d44102d24e8e7f4bc901d5e7f94a7bd86753c7
velutha <qwerty.amith@gmail.com>
2017-10-20 17:30:27 +0530

    persisting data fix

--- src/models/user.py
+++ src/models/user.py
@@ -1,5 +1,4 @@
 import uuid
-import pdb
 import json
 import time
 from src import app
@@ -14,7 +13,6 @@ class User():
         response = {}
         user_session_key = self.user_id + "." + "sessions"
         user_sessions = app.redis_client.lrange(user_session_key, 0 , -1)
-        # user_sessions = [str(session,'utf-8') for session in sessions]
 
         if session_id in user_sessions:
             self.session_id = session_id
@@ -34,23 +32,32 @@ class User():
         try:
             new_state = {}
             new_var_data = state.get("new_var_data", {})
+
             timestamp = int(time.time()) 
+            self._persist_data(var_data=new_var_data, session_id = session_id, timestamp = timestamp)
+
             old_state = app.redis_client.hgetall(session_id)
             current_var_data = old_state.get("var_data", "{}")
             new_var_data.update(json.loads(current_var_data))
+
             new_state["current_node_id"] = state["current_node_id"]
             new_state["timestamp"] = timestamp
             new_state["var_data"] = json.dumps(new_var_data)
-            # for key in new_var_data.keys():
-                # new_state[key] = new_var_data[key]
+
             app.redis_client.hmset(session_id, new_state)
-            db = app.couch["user_data"]
-            document = {"user_id" : self.user_id, "session_id": session_id, "data": new_state, "timestamp": timestamp}
+            return 1
+        except Exception as e:
+            print(e)
+            raise
+
+    def _persist_data(self, var_data = {} , session_id = "", timestamp = int(time.time())):
+        if (var_data == {}):
+            return 1
+        db = app.couch["user_data"]
+        document = {"user_id" : self.user_id, "session_id": session_id, "data": var_data, "timestamp": timestamp}
+        try:
             db.save(document)
             return 1
         except Exception as e:
             print(e)
-            pass
-
-    def create_session(self):
-        pass
+            raise
