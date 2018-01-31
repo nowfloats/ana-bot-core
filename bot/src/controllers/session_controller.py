62f941f543708ab803fd79ef578d06276ca1e919
velutha <qwerty.amith@gmail.com>
2018-01-31 19:27:31 +0530

    db shift initiated

--- bot/src/controllers/session_controller.py
+++ bot/src/controllers/session_controller.py
@@ -10,22 +10,21 @@ class SessionController():
     def __init__(self, *args, **kwargs):
         pass
 
-    @staticmethod
-    def clear_sessions(user_id):
-        # shift this to user model
-        user_key = user_id + "." + "sessions"
-        sessions = CACHE.lrange(user_key, 0, -1)
+    # @staticmethod
+    # def clear_sessions(user_id):
+        # user_key = user_id + "." + "sessions"
+        # sessions = CACHE.lrange(user_key, 0, -1)
 
-        if sessions == []:
-            return jsonify(message="no_sessions_found")
+        # if sessions == []:
+            # return jsonify(message="no_sessions_found")
 
-        sessions.append(user_key)
-        clear_sessions = CACHE.delete(*sessions)
+        # sessions.append(user_key)
+        # clear_sessions = CACHE.delete(*sessions)
 
-        if clear_sessions:
-            return jsonify(message="sessions_cleared")
+        # if clear_sessions:
+            # return jsonify(message="sessions_cleared")
 
-        return jsonify(message="could_not_clear_sessions"), 500
+        # return jsonify(message="could_not_clear_sessions"), 500
 
     @staticmethod
     def get_active_session(user_id, business_id, flow_id):
--- bot/src/models/business.py
+++ bot/src/models/business.py
@@ -1,91 +1,131 @@
 """
-Model for business entity, one bot is one business
+Model for business entity, one business can have many chat flows
+Author: https://github.com/velutha
 """
 import datetime
-import uuid
-import json
 from src import CACHE, DB
-from src.config import flow_config as config
 from src.logger import logger
+# import uuid
+# import json
+# from src.config import flow_config as config
 
 class Business():
+    """
+    Object blueprint in CACHE
+        {
+            business_id: {
+                "name": "",
+                "email": "",
+                "phone": "",
+                "logoUrl": "",
+                "status": "",
+                "colors": []
+            }
+        }
+    """
 
     def __init__(self, business_id):
 
         self.business_id = business_id
         self.CACHE = CACHE
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
-        self.collection = DB.business_data
+        self.collection = DB.Businesses
 
-    # all cache and db methods should merge together
-    # have persistent layer load cache if not found in cache
-    def get_business_data(self):
+    def save(self, business_data):
 
-        business_object = self.CACHE.hgetall(self.business_id)
-        return business_object
+        business_keys = ["name", "email", "phone", "logoUrl", "status", "colors"]
+        data = {key : business_data[key] for key in business_keys}
+        data["timestamp"] = datetime.datetime.now()
 
-    def get_details(self):
-        business_object = self.collection.find_one({"business_id": self.business_id})
-        if business_object is None:
+        try:
+            self.CACHE.hmset(self.business_id, data)
+            logger.info(f"Business data written to cache for {self.business_id}")
+            return True
+        except Exception as err:
+            logger.error(f"Error writing business data to cache {self.business_id}")
+            logger.error(err)
+            return False
+
+    def get_info(self):
+
+        # read from cache, if not found, read from db and save to cache
+        business_data = self.CACHE.hgetall(self.business_id)
+
+        if business_data is None:
+            business_data = self.collection.find_one({"_id": self.business_id})
+
+        if business_data is None:
             return {}
-        response_keys = ["business_id", "flow", "business_name", "flow_id"]
-        business_details = {key: value for key, value in business_object.items() if key in response_keys}
-        return business_details
 
-    def save_business_data_to_cache(self, business_data, nodes):
+        self.save(business_data)
 
-        if business_data == {}:
-            return False
+        return business_data
 
-        flow_id = business_data["flow_id"]
-        node_dict = {}
+    # def get_business_data(self):
 
-        business_keys = ["flow_id", "business_name"]
-        business_data_to_save = {key : business_data[key] for key in business_keys}
+        # business_object = self.CACHE.hgetall(self.business_id)
+        # return business_object
 
-        for node in nodes:
-            if node["Id"] not in config["archived_node_ids"]:
-                if node.get("IsStartNode", False):
-                    get_started_key = flow_id + "." + config["first_node_key"]
-                    node_dict[get_started_key] = json.dumps(node)
-                key = flow_id + "." + node["Id"]
-                node_dict[key] = json.dumps(node)
+    # def get_details(self):
+        # business_object = self.collection.find_one({"_id": self.business_id})
+        # if business_object is None:
+            # return {}
+        # response_keys = ["business_id", "flow", "business_name", "flow_id"]
+        # business_details = {key: value for key, value in business_object.items() if key in response_keys}
+        # return business_details
 
-        try:
-            self.CACHE.mset(node_dict)
-            logger.info(f"Node data written to cache for flow {flow_id}")
-            self.CACHE.hmset(self.business_id, business_data_to_save)
-            logger.info(f"Business data written to cache for flow {flow_id}")
-            return True
-        except Exception as err:
-            logger.error("Error writing to cache")
-            logger.error(err)
-            return False
+    # def save_business_data_to_cache(self, business_data, nodes):
 
-    def save(self, data):
+        # if business_data == {}:
+            # return False
 
-        match_query = {"business_id" : self.business_id}
+        # flow_id = business_data["flow_id"]
+        # node_dict = {}
 
-        update_document = {}
-        flow_id = str(uuid.uuid4())
-        update_document["flow"] = data["flow"]
-        update_document["business_name"] = data["business_name"]
-        update_document["updated_at"] = self.updated_at
+        # business_keys = ["flow_id", "business_name"]
+        # business_data_to_save = {key : business_data[key] for key in business_keys}
 
-        try:
-            self.collection.update_one(
-                match_query,
-                {
-                    "$set": update_document,
-                    "$setOnInsert": {
-                        "flow_id": flow_id,
-                        "created_at": self.created_at
-                    }
-                },
-                upsert=True)
+        # for node in nodes:
+            # if node["Id"] not in config["archived_node_ids"]:
+                # if node.get("IsStartNode", False):
+                    # get_started_key = flow_id + "." + config["first_node_key"]
+                    # node_dict[get_started_key] = json.dumps(node)
+                # key = flow_id + "." + node["Id"]
+                # node_dict[key] = json.dumps(node)
 
-            return True
-        except Exception as err:
-            logger.error(err)
-            return False
+        # try:
+            # self.CACHE.mset(node_dict)
+            # logger.info(f"Node data written to cache for flow {flow_id}")
+            # self.CACHE.hmset(self.business_id, business_data_to_save)
+            # logger.info(f"Business data written to cache for flow {flow_id}")
+            # return True
+        # except Exception as err:
+            # logger.error("Error writing to cache")
+            # logger.error(err)
+            # return False
+
+    # def save(self, data):
+
+        # match_query = {"business_id" : self.business_id}
+
+        # update_document = {}
+        # flow_id = str(uuid.uuid4())
+        # update_document["flow"] = data["flow"]
+        # update_document["business_name"] = data["business_name"]
+        # update_document["updated_at"] = self.updated_at
+
+        # try:
+            # self.collection.update_one(
+                # match_query,
+                # {
+                    # "$set": update_document,
+                    # "$setOnInsert": {
+                        # "flow_id": flow_id,
+                        # "created_at": self.created_at
+                    # }
+                # },
+                # upsert=True)
+
+            # return True
+        # except Exception as err:
+            # logger.error(err)
+            # return False
